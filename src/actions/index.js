4ec6d637c615347132a8daa98d9a80a21d877e50
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-02-16 15:37:45 -0300

    reiniciando proje to em novo repositorio copiando algumas partes do anterior

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+export const SAVE_USER_EMAIL = 'SAVE_USER';
+export const saveUserAction = (payload) => ({
+  type: SAVE_USER_EMAIL,
+  payload,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisabled } = this.state;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="alguem@alguem.com"
+              data-testid="email-input"
+              onChange={ this.hadleChange }
+              value={ email }
+            />
+          </label>
+          password:
+          <label htmlFor="password">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="6 ou mais caracteres"
+              data-testid="password-input"
+              onChange={ this.hadleChange }
+              value={ password }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ isDisabled }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
