bcd5b0ab8008beed5a50bccad9c47330ee7ff6ef
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-02-16 16:02:53 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveUserAction } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+  }
+
+  hadleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, () => this.validationInputs());
+  };
+
+  validationInputs = () => {
+    const { email, password } = this.state;
+    const PASSWORD_LENGTH = 6;
+    // https://backefront.com.br/validacao-email-javascript/ 10/02/22
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (emailRegex.test(email) && password.length >= PASSWORD_LENGTH) {
+      return this.setState({
+        isDisabled: false,
+      });
+    }
+    this.setState({ isDisabled: true });
+  };
+
+  handleClick = () => {
+    const { history: { push }, saveUserDispatch } = this.props;
+    const { email } = this.state;
+    saveUserDispatch(email);
+    return push('/carteira');
+  };
+
   render() {
     const { email, password, isDisabled } = this.state;
     return (
@@ -43,4 +82,15 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  saveUserDispatch: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  saveUserDispatch: (payload) => dispatch(saveUserAction(payload)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
