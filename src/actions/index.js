e014dec8729d846360e6ac52e3cb5603e0a210b8
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-02-16 19:00:26 -0300

    requisito 4 criado actions SAVE_WALLET E FETCH

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,15 @@ export const saveUserAction = (payload) => ({
   type: SAVE_USER_EMAIL,
   payload,
 });
+
+export const SAVE_WALLET = 'SAVE_WALLET';
+export const wallet = (payload) => ({
+  type: SAVE_WALLET,
+  payload,
+});
+
+export const FETCH = 'FETCH';
+export const Fetch = (payload) => ({
+  type: FETCH,
+  payload,
+});
